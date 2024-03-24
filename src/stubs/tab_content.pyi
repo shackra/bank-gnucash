022dec351ac52f9ecbb61ac0dbff5ae26634882b
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-03-24 15:02:34 -0600

    Genera stubs

---
+++ src/stubs/tab_content.pyi
@@ -0,0 +1,34 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from _typeshed import Incomplete
+
+class Ui_Form:
+    verticalLayout: Incomplete
+    information: Incomplete
+    bank_name: Incomplete
+    currency: Incomplete
+    horizontalSpacer: Incomplete
+    initial_balance: Incomplete
+    horizontalLayout: Incomplete
+    verticalLayout_2: Incomplete
+    label: Incomplete
+    comboBox: Incomplete
+    verticalLayout_3: Incomplete
+    label_2: Incomplete
+    comboBox_2: Incomplete
+    verticalLayout_4: Incomplete
+    label_3: Incomplete
+    comboBox_3: Incomplete
+    verticalLayout_5: Incomplete
+    label_4: Incomplete
+    comboBox_4: Incomplete
+    verticalLayout_6: Incomplete
+    label_5: Incomplete
+    comboBox_5: Incomplete
+    verticalLayout_7: Incomplete
+    label_6: Incomplete
+    comboBox_6: Incomplete
+    statement: Incomplete
+    def setupUi(self, Form) -> None: ...
+    def retranslateUi(self, Form) -> None: ...
---
+++ src/stubs/window.pyi
@@ -0,0 +1,14 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from _typeshed import Incomplete
+
+class Ui_MainWindow:
+    centralwidget: Incomplete
+    verticalLayout_2: Incomplete
+    horizontalLayout: Incomplete
+    horizontalSpacer: Incomplete
+    add_bank_statement: Incomplete
+    bank_data: Incomplete
+    def setupUi(self, MainWindow) -> None: ...
+    def retranslateUi(self, MainWindow) -> None: ...
